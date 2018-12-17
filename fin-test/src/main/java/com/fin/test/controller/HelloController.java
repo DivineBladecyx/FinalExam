80698f1dff36adf77a4f0c2ab359aeafa6eeed92
DivineBlade <your_registered_github_Email>
2018-12-17 22:11:28 +0800

    更改

--- fin-test/src/main/java/com/fin/test/controller/HelloController.java
+++ fin-test/src/main/java/com/fin/test/controller/HelloController.java
@@ -1,14 +1,16 @@
 package com.fin.test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class HelloController {
-
-    @RequestMapping("/hello")
-    private String index(){
-        return "Hello World!";
+    @RequestMapping(value = {"/t","/temp","/template"},method = RequestMethod.GET)
+    public String showIndexHtml(){
+        return "index";
     }
+
 }
