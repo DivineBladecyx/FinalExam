609f8a46aaa800abe7f8749ec794d734a97fae60
DivineBlade <your_registered_github_Email>
2018-12-18 22:31:47 +0800

    ggg

--- fin-test/src/main/java/com/fin/test/controller/HtmlController.java
+++ fin-test/src/main/java/com/fin/test/controller/HtmlController.java
@@ -4,15 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 
 @Controller
-public class HelloController {
+public class HtmlController {
     @GetMapping("/html1")
     public String html1() {
         return "/index1.html";
