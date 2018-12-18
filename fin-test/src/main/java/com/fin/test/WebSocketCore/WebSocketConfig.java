159f11b94dd548056fb384aadd8b19f6c0e8ce3a
DivineBlade <your_registered_github_Email>
2018-12-18 21:38:00 +0800

    基本能够实现实时聊天

---
+++ fin-test/src/main/java/com/fin/test/WebSocketCore/WebSocketConfig.java
@@ -0,0 +1,13 @@
+package com.fin.test.WebSocketCore;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
--- fin-test/src/main/java/com/fin/test/controller/HelloController.java
+++ fin-test/src/main/java/com/fin/test/controller/HelloController.java
@@ -2,15 +2,23 @@ package com.fin.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
-@EnableAutoConfiguration
-public class HelloController {
-    @RequestMapping(value = {"/t","/temp","/template"},method = RequestMethod.GET)
-    public String showIndexHtml(){
-        return "index";
-    }
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+
+@Controller
+public class HelloController {
+    @GetMapping("/html1")
+    public String html1() {
+        return "/index1.html";
+    }
+    @GetMapping("/html2")
+    public String html2() {
+        return "/index2.html";
+    }
 }
